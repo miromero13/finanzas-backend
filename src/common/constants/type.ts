7c6321a98cf8d560b9b1da314d0ffe9ba1544e31
Ilse Romero <ilseromero35@gmail.com>
2024-06-29 12:41:20 -0400

    feat: add constants

---
+++ src/common/constants/type.ts
@@ -0,0 +1,21 @@
+export enum COMPANY_TYPE {
+  INDUSTRIAL = 'industrial',
+  COMMERCIAL = 'comercial',
+  SERVICES = 'servicios',
+  OTHER = 'otro',
+}
+
+export enum INTEREST_TYPE {
+  SIMPLE = 'simple',
+  COMPOUND = 'compuesto',
+}
+
+export enum DISCOUNT_TYPE {
+  SIMPLE = 'simple',
+  COMPOUND = 'compuesto',
+}
+
+export enum BALANCE_TYPE {
+  MONTHLY = 'mensual',
+  ANNUAL = 'anual',
+}
